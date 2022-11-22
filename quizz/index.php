ad81f23581c74fb2824ec8e1f53c39a5eda7bbba
DannyChronos <dayanetrunks@gmail.com>
2022-11-22 21:02:04 +0100

    dayo

--- quizz/index.php
+++ quizz/index.php
@@ -7,6 +7,8 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Le Pays trop Mignon</title>
+   <!--favicon-->
+   <link rel="shortcut icon" href="../img/logo/images-removebg-preview.ico" type="image/x-icon">
    <!-- FontAwesome-cdn include -->
    <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
    <!-- Google fonts include -->
